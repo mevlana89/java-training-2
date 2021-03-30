043217fc9c5a7a681a925daf5713defdf8e29126
ergunmu1 <mustafa.ergun@natixis.com>
2021-03-30 14:39:14 +0200

    quete_training 2

--- tester.sh
+++ tester.sh
@@ -4,14 +4,14 @@ if [ -n "$1" ]; then
     classname="$1"
     classname="$(tr '[:lower:]' '[:upper:]' <<< ${classname:0:1})${classname:1}"
 
-    javac -classpath out:lib/junit-4.12.jar -d out src/exercices/${classname}.java 2> logs/checkmethod_output.txt
+    /c/work/apps/jdk8u202-b08/bin/javac -classpath lib/junit-4.12.jar -d out src/exercices/${classname}.java 2> logs/checkmethod_output.txt
     if [[ $(< logs/checkmethod_output.txt) != "" ]]; then
         cat logs/checkmethod_output.txt
     else
-        javac -classpath out:lib/junit-4.12.jar -d out src/tests/MainTest.java
-        javac -classpath out:lib/junit-4.12.jar -d out src/tests/${classname}Test.java
+        /c/work/apps/jdk8u202-b08/bin/javac -classpath lib/junit-4.12.jar -d out src/tests/MainTest.java
+        /c/work/apps/jdk8u202-b08/bin/javac -classpath lib/junit-4.12.jar -d out src/tests/${classname}Test.java
 
-        java -classpath out:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore ${classname}Test
+        /c/work/apps/jdk8u202-b08/bin/java -classpath lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore ${classname}Test
     fi
 else
     echo "Please specify the class to test, e.g. : ./tester.sh PrimitiveTraining"
